54b4557764784ab25f6281c315be5bd13f178a33
korakod.s <korakod.s@entronica.co.th>
2019-11-28 12:08:28 +0700

    Add card buttons

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,5 +3,11 @@ console.log("Trello test power up");
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         return []
-    }
+    },
+    'card-buttons': function(t, options){
+        return [{
+          icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+          text: 'Estimate Size',
+        }];
+    },
 });
