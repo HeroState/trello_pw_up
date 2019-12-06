f98c24a866d8e4d8299a3301d99b6c3c1c31ed87
korakod.s <korakod.s@entronica.co.th>
2019-12-06 10:35:28 +0700

    update btn chart

--- src/js/connector.js
+++ src/js/connector.js
@@ -9,7 +9,18 @@ window.TrelloPowerUp.initialize({
           icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
           text: 'Estimate Size',
           callback: onBtnClick,
-        }];
+        },
+        {
+            icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+            text: 'Burn Down Chart',
+            callback: function(t){
+              return t.popup({
+                title: "Burn Down Chart",
+                url: '../html/chart.html'
+              });
+            }
+          }
+    ];
     },
 });
 
