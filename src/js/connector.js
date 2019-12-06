c68f9726ab13c19670d47c3c89db550c0ee53cdb
korakod.s <korakod.s@entronica.co.th>
2019-12-06 13:53:45 +0700

    update btn chart

--- src/js/connector.js
+++ src/js/connector.js
@@ -9,9 +9,9 @@ window.TrelloPowerUp.initialize({
             light: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
           },
           text: 'Burn Down Chart',
-          url: 'https://trello-pw-up.netlify.com/chart.html',
+        //   url: 'https://trello-pw-up.netlify.com/chart.html',
+          callback: onBtnClick,
           condition: 'edit',
-          target: 'Chart',
 
         }
         , 
