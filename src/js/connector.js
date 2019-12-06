4af5d0a7ad57685f0959f59e81e97ec4c3263212
korakod.s <korakod.s@entronica.co.th>
2019-12-06 13:58:19 +0700

    update btn chart

--- src/js/connector.js
+++ src/js/connector.js
@@ -54,6 +54,23 @@ window.TrelloPowerUp.initialize({
 
 var onBtnClick = function (t, opts) {
     console.log('Someone clicked the button');
+    t.modal({
+        // the url to load for the iframe
+        url: './modal.html',
+        // optional arguments to be passed to the iframe as query parameters
+        // access later with t.arg('text')
+        args: { text: 'Hello' },
+        // optional color for header chrome
+        accentColor: '#F2D600',
+        // initial height for iframe
+        height: 500, // not used if fullscreen is true
+        // whether the modal should stretch to take up the whole screen
+        fullscreen: true,
+        // optional function to be called if user closes modal (via `X` or escape, etc)
+        callback: () => console.log('Goodbye.'),
+        // optional title for header chrome
+         
+    })
     t.popup({
         type: 'confirm',
         title: 'confirm',
