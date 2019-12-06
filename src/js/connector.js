62e021b248253cd461f30c986718e706bdc1d2d9
korakod.s <korakod.s@entronica.co.th>
2019-12-06 10:48:31 +0700

    add brond btn

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,33 @@
 
 console.log("Trello test power up");
 window.TrelloPowerUp.initialize({
+    'board-buttons': function (t, opts) {
+        return [{
+          // we can either provide a button that has a callback function
+          icon: {
+            dark: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+            light: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
+          },
+          text: 'Burn Down Chart',
+          url: 'chart.html',
+          condition: 'edit',
+          target: 'Inspiring Boards'
+        }
+        , 
+        // {
+        //     // or we can also have a button that is just a simple url
+        //     // clicking it will open a new tab at the provided url
+        //     icon: {
+        //         dark: WHITE_ICON,
+        //         light: BLACK_ICON
+        //     },
+        //     text: 'URL',
+        //     condition: 'always',
+        //     url: 'https://trello.com/inspiration',
+        //     target: 'Inspiring Boards' // optional target for above url
+        //     }
+        ];
+    },
     'card-badges': function (t, opts) {
         return []
     },
